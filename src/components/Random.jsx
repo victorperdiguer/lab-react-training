b90f3a8aeb386d5dbb539263500beeb89404f500
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-02-28 11:53:55 +0100

    iteration 3 done

---
+++ src/components/Random.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Random = (props) => {
+    const { min, max } = props;
+    const randNumber = Math.floor(Math.random() * (max - min) ) + min;
+    return (
+        <div className="random-div">
+            <p className="random-text">
+                Random value between {min} and {max} => {randNumber}
+            </p>
+        </div>
+    )
+}
+
+export default Random;
